2e79d6bd8052c526ad3eabc8600a023150e6262d
George Rushton <grushton94@yahoo.com>
2018-11-28 09:33:18 +0000

    jenkins got very slow adding echos to test why

--- HBase_scripts/Drop_Tables.sh
+++ HBase_scripts/Drop_Tables.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 #file="Drop_Tables.txt"
 
-echo "$3$1:enterprise_$2/schema"
+echo "curl -k -s -w '%{http_code}' --header 'Content-type: text/xml' --request DELETE $3$1:enterprise_$2/schema"
 response=$(curl -k -s -w "%{http_code}" --header "Content-type: text/xml" --request DELETE "$3$1:enterprise_$2/schema")
+echo $response
+echo "curl -k -s -w '%{http_code}' --header 'Content-type: text/xml' --request DELETE $3$1:enterprise_$2/schema"
 response=$(curl -k -s -w "%{http_code}" --header "Content-type: text/xml" --request DELETE "$3$1:legal_unit_$2/schema")
+echo $response
+echo "curl -k -s -w '%{http_code}' --header 'Content-type: text/xml' --request DELETE $3$1:enterprise_$2/schema"
 response=$(curl -k -s -w "%{http_code}" --header "Content-type: text/xml" --request DELETE "$3$1:local_unit_$2/schema")
+echo $response
+echo "curl -k -s -w '%{http_code}' --header 'Content-type: text/xml' --request DELETE $3$1:enterprise_$2/schema"
 response=$(curl -k -s -w "%{http_code}" --header "Content-type: text/xml" --request DELETE "$3$1:reporting_unit_$2/schema")
+echo $response
+echo "curl -k -s -w '%{http_code}' --header 'Content-type: text/xml' --request DELETE $3$1:enterprise_$2/schema"
 response=$(curl -k -s -w "%{http_code}" --header "Content-type: text/xml" --request DELETE "$3$1:unit_link_$2/schema")
+echo $response
+echo "curl -k -s -w '%{http_code}' --header 'Content-type: text/xml' --request DELETE $3$1:enterprise_$2/schema"
 if [[ "$4" != r ]] ; then
 	response=$(curl -k -s -w "%{http_code}" --header "Content-type: text/xml" --request DELETE "$3$1:vat/schema")
 	response=$(curl -k -s -w "%{http_code}" --header "Content-type: text/xml" --request DELETE "$3$1:paye/schema")
